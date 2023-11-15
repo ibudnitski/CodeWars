cc139e6ed6ea780e23b4ca09308e9bf0163ab0b0
ibudnitski <igor.budnitski@gmail.com>
2023-11-15 20:34:21 +0200

    Commit

---
+++ Objects.js
@@ -0,0 +1,22 @@
+let person1 = {
+    name: 'Igor',
+    age: 35,
+}
+
+let person2 = {
+    name: 'Anton',
+    age: 34,
+}
+
+let person3 = {
+    name: 'Artem',
+    age: 45,
+}
+
+let person4 = {
+    name: 'Andrei',
+    age: 12,
+}
+
+
+console.log(person1, person2, person3, person4);
